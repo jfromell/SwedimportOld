3d4cd20caf12f467ad8e1b3e635bf533d73777b1
JonasFromell <feedtheplague@gmail.com>
2011-12-02 10:50:52 +0100

    more changes to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,14 +21,16 @@ role :web, "31.192.226.123"                          # Your HTTP server, Apache/
 role :app, "31.192.226.123"                          # This may be the same as your `Web` server
 role :db,  "31.192.226.123", :primary => true # This is where Rails migrations will run
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do
+    sudo "/etc/init.d/unicorn start"
+  end
+  
+  task :stop do
+    sudo "/etc/init.d/unicorn stop"
+  end
+  
+  task :restart do
+    sudo "/etc/init.d/unicorn reload"
+  end
+end
