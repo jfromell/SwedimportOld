f3b4e25925e25d5c11519c05486a5c28e18a36be
JonasFromell <feedtheplague@gmail.com>
2011-12-05 12:06:46 +0100

    Changed some unicorn attribs.

--- unicorn.rb
+++ unicorn.rb
@@ -1,4 +1,4 @@
-worker_processes 2
+worker_processes 4
 
 working_directory "/home/deployer/rails/current"
 
@@ -8,7 +8,7 @@ timeout 30
 user 'deployer', 'staff'
 
 shared_path = "/home/deployer/rails/shared"
-pid = "#{shared_path}/pids/unicorn.pid"
+pid "#{shared_path}/pids/unicorn.pid"
 
 stderr_path "#{shared_path}/log/unicorn.stderr.log"
 stdout_path "#{shared_path}/log/unicorn.stdout.log"
