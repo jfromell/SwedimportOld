053f8c80692af25005fec36b387af3d291e9369f
JonasFromell <feedtheplague@gmail.com>
2011-12-05 12:34:49 +0100

    Deleted some files from repo

--- db/migrate/backups/20110429081040_add_names_depth_to_categories.rb
+++
@@ -1,9 +0,0 @@
-class AddNamesDepthToCategories < ActiveRecord::Migration
-  def self.up
-    add_column :categories, :names_depth_cache, :string
-  end
-
-  def self.down
-    remove_column :categories, :names_depth_cache
-  end
-end
--- db/migrate/backups/20110504082116_add_depth_to_categories.rb
+++
@@ -1,9 +0,0 @@
-class AddDepthToCategories < ActiveRecord::Migration
-  def self.up
-    add_column :categories, :ancestry_depth, :integer, :default => 0
-  end
-
-  def self.down
-    remove_column :categories, :ancestry_depth
-  end
-end
