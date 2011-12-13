a25f8a5f65b60b71ac6e019f1c59f2293741f379
JonasFromell <feedtheplague@gmail.com>
2011-12-13 16:11:55 +0100

    alksdjf

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
   def contact_us(contact_message)
     @contact_message = contact_message
     
-    mail(:to => info@swedimport.se, :from => contact_message.email, :subject => contact_message.subject)
+    mail(:to => "info@swedimport.se", :from => contact_message.email, :subject => contact_message.subject)
   end
   
 end
