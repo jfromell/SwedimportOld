9a6a9fe752d9acf2db69d8f5bb4990b8ea18ee10
JonasFromell <feedtheplague@gmail.com>
2011-12-13 11:58:55 +0100

    getting ready for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,21 @@
-# load 'deploy/assets'
+set :use_sudo,            false
 
-set :use_sudo,              false
+set :scm,                 :git
+set :git_shallow_clone,   1
+set :keep_releases,       3
+set :repository,          "git@github.com:JonasFromell/swedimport.git"
 
-set :git_shallow_clone,     false
+set :application,         "swedimport"
 
-set :keep_releases,         3
+set :user,                "deployer"
+set :password,            "8w2u7757"
 
-set :application,           "swedimport"
+set :deploy_to,           "/home/deployer/swedimport"
 
-set :user,                  "deployer"
+set :runner,              "deployer"
 
-set :password,              "8w2u7757"
+set :real_revision,       lambda { source.query_revision(revision) { |cmd| capture(cmd) } }
 
-set :deploy_to,             "/home/deployer/swedimport"
-
-set :runner,                "deployer"
-
-set :repository,            "git@github.com:JonasFromell/swedimport.git"
-set :scm,                   :git
-set :real_revision,         lambda { source.query_revision(revision) { |cmd| capture(cmd) } }
-
-ssh_options[:paranoid]      = false
-default_run_options[:pty]   = true
-
-role :app,                  "31.192.226.123"
-role :web,                  "31.192.226.123"
-role :db,                   "31.192.226.123", :primary => true
-
-namespace :deploy do
-  task :start do
-    sudo "/etc/init.d/unicorn start"
-  end
-  task :stop do
-    sudo "/etc/init.d/unicorn stop"
-  end
-  task :restart do
-    sudo "/etc/init.d/unicorn reload"
-  end
-end
+role :app,                "31.192.226.123"
+role :web,                "31.192.226.123"
+role :db,                 "31.192.226.123", :primary => true
--- unicorn.rb
+++ unicorn.rb
@@ -2,10 +2,12 @@ worker_processes 4
 working_directory "/home/deployer/swedimport/current"
 
 listen "/tmp/swedimport.socket", :backlog => 64
+
 timeout 30
 
 user 'deployer', 'staff'
+
 shared_path = "/home/deployer/swedimport/shared"
-pid "#{shared_path}/pids/unicorn.pid"
+pid = "#{shared_path}/log/unicorn.pid"
 stderr_path "#{shared_path}/log/unicorn.stderr.log"
 stdout_path "#{shared_path}/log/unicorn.stdout.log"
