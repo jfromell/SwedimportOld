43c3e6f646cfa99f041184555c03d5d6a8c20e31
JonasFromell <feedtheplague@gmail.com>
2011-12-28 08:51:56 +0100

    more backedn issues

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ class Admin::CategoriesController < Admin::BaseController
   before_filter :authenticate_admin!
   
   def index
-    @categories = Category.all
+    @categories = Category.all.arrange(:order => :name)
   end
   
   def show
