c4813c10ee3378c3160b1f28fbb9503a9108aec4
JonasFromell <feedtheplague@gmail.com>
2011-12-28 16:15:33 +0100

    cleaning

--- app/controllers/contacts_controller.rb
+++
@@ -1,9 +0,0 @@
-class ContactsController < ApplicationController
-   
-  def create
-    @contact_message = Contact.new(params[:contact_message])
-    @contact_message.save
-    flash[:notice] = 'Ditt meddelande har skickats!'
-  end
-  
-end
--- app/mailers/notifier.rb
+++
@@ -1,9 +0,0 @@
-class Notifier < ActionMailer::Base
-  
-  def contact_us(contact_message)
-    @contact_message = contact_message
-    
-    mail(:to => "info@swedimport.se", :from => contact_message.email, :subject => contact_message.subject)
-  end
-  
-end
